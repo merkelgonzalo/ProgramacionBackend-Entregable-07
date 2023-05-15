f16c8482ee407b1dffca2eb778c1af2a3c2b7964
Gonzalo Merkel <merkelgonzalo.ok@gmail.com>
2023-05-15 18:40:58 -0300

    GET Cart with all products' information

--- src/Dao/models/carts.model.js
+++ src/Dao/models/carts.model.js
@@ -17,8 +17,8 @@ const cartSchema = new mongoose.Schema({
     },
 });
 
-// cartSchema.pre('find', function(){
-//     this.populate("products.product");
-// })
+cartSchema.pre('find', function(){
+    this.populate("products.product");
+})
 
 export const cartModel = mongoose.model(cartCollection, cartSchema);
