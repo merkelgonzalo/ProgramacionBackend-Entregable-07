a8e9799331b320a00556eab60655ab4f872d87a5
Gonzalo Merkel <merkelgonzalo.ok@gmail.com>
2023-05-16 20:44:43 -0300

    Products view with paginate

--- src/Dao/models/products.model.js
+++ src/Dao/models/products.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
 
 const productCollection = 'products'
 
@@ -24,5 +25,7 @@ const productSchema = new mongoose.Schema({
     },
     status: Boolean
 });
+
+productSchema.plugin(mongoosePaginate);
 
 export const productModel = mongoose.model(productCollection, productSchema);
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,12 +1,20 @@
 import {Router} from 'express';
-//import ProductManager from '../Dao/managers/ProductManager.js';
 import { productModel } from '../Dao/models/products.model.js';
 
 const router = Router();
 
 router.get('/', async (req, res) => { 
-    const products = await productModel.find().lean();
-    res.render('home', {products: products});
+    const {page = 1} = req.query;
+    //const products = await productModel.find().lean();
+    const {docs, hasPrevPage, hasNextPage, nextPage, prevPage} = await productModel.paginate({},{limit:2, page, lean:true })
+    const products = docs;
+    res.render('home', {
+        products: products,
+        hasPrevPage,
+        hasNextPage,
+        prevPage,
+        nextPage
+    });
 });
 
 router.get('/realTimeProducts', async (req, res) => { 
